12ee31456fcef8d9abea33a3ffd97e2c055d34b1
Luke VanderHart <luke@vanderhart.net>
2017-04-28 12:07:53 -0400

    add logging

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [reloaded.repl "0.2.3"]
                  [org.arachne-framework/arachne-pedestal "0.2.0-master-0061-e16dc3d"]
-                 [datascript "0.16.1"]]
+                 [datascript "0.16.1"]
+                 [ch.qos.logback/logback-classic "1.1.3"]]
   :source-paths ["src" "config"]
   :profiles {:dev {:source-paths ["src" "dev" "config"]}}
   :repositories [["arachne-dev"
